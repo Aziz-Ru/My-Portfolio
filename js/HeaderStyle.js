d86e2bcdd0607e6e37a06f8f705fc356296eb5e5
Aziz <siliconoxide4@gmail.com>
2024-05-15 21:05:01 +0600

    Added AOS

--- js/HeaderStyle.js
+++ js/HeaderStyle.js
@@ -3,6 +3,7 @@ window.onscroll = () => {
   console.log("hello");
   if (document.body.scrollTop > 100) {
     navigation.style.top = "absolute";
+    navigation.style.backgroundColor = "transparent";
   } else {
     navigation.style.position = "fixed";
     navigation.style.backgroundColor = "#ffffff";
