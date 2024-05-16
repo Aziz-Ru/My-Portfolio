61a4b6e310322e3ad6e6f9900a6ba101a1132fb9
Aziz <siliconoxide4@gmail.com>
2024-05-16 23:44:01 +0600

    Added projects

---
+++ js/index.js
@@ -0,0 +1,41 @@
+const TypingText = document.getElementById("typing-text");
+const allTypedText = [
+  "A Web Developer.",
+  "A Photographer.",
+  "A Web Desginer.",
+  "A Backend Developer",
+];
+
+let alltypeTextCnt = 0;
+let text = allTypedText[alltypeTextCnt];
+let i = 0,
+  cnt = 0;
+
+function typeWriting() {
+  if (alltypeTextCnt < allTypedText.length) {
+    if (i < text.length && cnt == 0) {
+      if (i == text.length - 1) cnt = 1;
+      TypingText.innerHTML += text.charAt(i);
+      i++;
+      setTimeout(typeWriting, 300);
+    } else if (cnt == 1) {
+      if (i == 0) {
+        cnt += 1;
+        // alltypeTextCnt += 1;
+      }
+      const viewString = TypingText.innerHTML;
+      TypingText.innerHTML = viewString.substring(0, i);
+      i--;
+      setTimeout(typeWriting, 200);
+    } else if (cnt == 2) {
+      alltypeTextCnt += 1;
+      if (alltypeTextCnt == allTypedText.length) {
+        alltypeTextCnt = 0;
+      }
+      cnt = 0;
+      text = allTypedText[alltypeTextCnt];
+      setTimeout(typeWriting, 200);
+    }
+  }
+}
+typeWriting();
