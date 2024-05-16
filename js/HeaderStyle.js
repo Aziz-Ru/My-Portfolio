b4a40d19f899196ad81b78961b918e6d9064d095
Aziz <siliconoxide4@gmail.com>
2024-05-16 23:57:21 +0600

    Added projects

--- js/HeaderStyle.js
+++ js/HeaderStyle.js
@@ -1,6 +1,5 @@
 const navigation = document.getElementById("navigation");
 window.onscroll = () => {
-  console.log("hello");
   if (document.body.scrollTop > 100) {
     navigation.style.top = "absolute";
     navigation.style.backgroundColor = "transparent";
